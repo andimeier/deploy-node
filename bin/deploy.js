1615b7e43da1bb035a9c11097b65a42d1a612814
Alexander Eck-Zimmer <alexander.eck-zimmer@siemens.com>
2019-05-08 14:06:01 +0200

    update libs, update outdated dependencies

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,19 +1,17 @@
 #! /usr/bin/env node
-var path = require('path');
-var ansi = require('ansi-escape-sequences');
-var cmdLineArgs = require('../lib/options');
-var log = require('../lib/log');
-var folders = require('../lib/folders');
-var npm = require('../lib/npm');
-var version = require('../lib/version');
+var path = require("path");
+var ansi = require("ansi-escape-sequences");
+var cmdLineArgs = require("../lib/options");
+var log = require("../lib/log");
+var folders = require("../lib/folders");
+var npm = require("../lib/npm");
+var version = require("../lib/version");
 
 var options;
-
 
 options = cmdLineArgs.processCommandLineArgs();
 
 //Xeyes.inspect(options);
-
 
 folders.clearBuildBaseFolder(options.buildBaseFolder);
 
@@ -22,12 +20,22 @@ folders.copySourceFiles(options.sourceFolder, options.buildFolder);
 npm.installDependencies(options.buildFolder);
 
 if (options.versionFile) {
-    version.createVersionsFile(path.join(options.buildFolder, options.versionFile), options.version, options.message);
+  version.createVersionsFile(
+    path.join(options.buildFolder, options.versionFile),
+    options.version,
+    options.message
+  );
 } else {
-    log.log('no version file generated (no filename given).');
+  log.log("no version file generated (no filename given).");
 }
 
-folders.package(options.buildFolder, options.deploymentFile, function () {
-    log.log(ansi.format('deployment package [green]{' + options.deploymentFile + '} ready to be deployed'));
-    log.success('Build finished.');
+folders.package(options.buildFolder, options.deploymentFile).then(() => {
+  log.log(
+    ansi.format(
+      "deployment package [green]{" +
+        options.deploymentFile +
+        "} ready to be deployed"
+    )
+  );
+  log.success("build finished.");
 });
